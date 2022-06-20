e16ecf387ab9ee29e1f73d5c2ff15f4774632514
bfrazier <bthehippie2@gmail.com>
2022-06-20 17:28:06 -0400

    complete js for home score counter.

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let homeScore = document.getElementById('home-score')
-let awayScore = document.getElementById('away-score')
+let homeScoreEl = document.getElementById('home-score')
+let homeScore = 0
 
 
+function addOneHome(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
 
+}
+
+function addTwoHome(){
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+function addThreeHome(){
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+let awayScoreEl = document.getElementById('away-score')
+let awayScore = 0
+
+function addOneAway(){
+    awayScore += 1
+    awayScoreEl.textContent = awayScore
+}
 
